37e491c1bdd8a2c402d6feb6e61fc101cfaaec05
anselnettles <aviholai@student.hive.fi>
2022-09-30 15:32:04 +0300

    Oh my god, it's drawing lines now.

--- extract_03.c
+++ extract_03.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 11:54:01 by aviholai          #+#    #+#             */
-/*   Updated: 2022/09/30 14:50:10 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/09/30 15:24:50 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,20 @@ int	draw_line(t_vars *v)
 	float	f_y_pos;
 	float	f_prev_x;
 	float	f_prev_y;
-	//float	amount;
+	float	amount;
+	double	step;
 
 	f_x_pos = v->x_pos;
 	f_y_pos = v->y_pos;
 	f_prev_x = v->prev_x;
 	f_prev_y = v->prev_y;
 	printf("\nPrev X = %d. Prev Y = %d. Now X = %d. Now Y = %d", v->prev_x, v->prev_y, v->x_pos, v->y_pos);
+	step = INCREMENT / 1.5;
+	amount = ((f_y_pos - f_prev_y) / (float)step);
 	while (f_prev_x <= f_x_pos)
 	{
 		f_prev_x++;
-		f_prev_y += ((f_y_pos - f_prev_y) / INCREMENT);
+		f_prev_y += amount;
 		v->prev_x = (int)f_prev_x;
 		v->prev_y = (int)f_prev_y;
 		mlx_pixel_put(v->mlx, v->win, v->prev_x, v->prev_y, v->color);
